63b32991c089faaf6f41f0542083758f326849d9
Ychnightder <pierreychnightder561@gmail.com>
2026-06-02 01:37:59 +0200

    refactor: remove unused Prisma configuration and clean up image upload logic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -35,20 +35,20 @@ export default function Home() {
 			const formData = new FormData();
 			formData.append('file', file);
 
-			const response = await fetch(`${NEXT_PUBLIC_URL_BACKEND}/upload`, {
-				method: 'POST',
-				body: formData,
-			});
+			// const response = await fetch(`${NEXT_PUBLIC_URL_BACKEND}/upload`, {
+			// 	method: 'POST',
+			// 	body: formData,
+			// });
 
-			if (!response.ok) throw new Error('Erreur');
+			// // if (!response.ok) throw new Error('Erreur');
 
-			const data = await response.json();
+			// // const data = await response.json();
 
-      // const data = {
-			// 	success: true,
-			// 	result:
-			// 		"<div class='flex justify-center items-center h-screen bg-gray-200'> <div class='bg-white p-8 rounded-lg shadow-md w-full max-w-lg mx-auto'> <h2 class='text-3xl font-bold mb-4 text-dark-green'>Contact Us</h2> <form class='flex flex-col gap-6'> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>First Name Last Name *</label> <div class='flex gap-4 flex-wrap sm:flex-nowrap'> <input type='text' placeholder='First Name' class='w-full sm:w-1/2 p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> <input type='text' placeholder='Last Name' class='w-full sm:w-1/2 p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> </div> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Email Address</label> <input type='email' placeholder='Email Address' class='w-full p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Query Type</label> <div class='flex gap-4 flex-wrap'> <div class='flex items-center gap-2'> <input type='radio' id='general-enquiry' name='query-type' class='form-radio'> <label for='general-enquiry' class='text-sm text-gray-600'>General Enquiry</label> </div> <div class='flex items-center gap-2'> <input type='radio' id='support-request' name='query-type' class='form-radio'> <label for='support-request' class='text-sm text-gray-600'>Support Request</label> </div> </div> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Message *</label> <textarea placeholder='Message' class='w-full p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'></textarea> </div> <div class='flex items-center gap-2'> <input type='checkbox' id='consent' class='form-checkbox'> <label for='consent' class='text-sm text-gray-600'>I consent to being contacted by the team *</label> </div> <button type='submit' class='bg-dark-green text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-300'>Submit</button> </form> </div> </div>",
-			// };
+       const data = {
+			 	success: true,
+			 	result:
+			 		"<div class='flex justify-center items-center h-screen bg-gray-200'> <div class='bg-white p-8 rounded-lg shadow-md w-full max-w-lg mx-auto'> <h2 class='text-3xl font-bold mb-4 text-dark-green'>Contact Us</h2> <form class='flex flex-col gap-6'> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>First Name Last Name *</label> <div class='flex gap-4 flex-wrap sm:flex-nowrap'> <input type='text' placeholder='First Name' class='w-full sm:w-1/2 p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> <input type='text' placeholder='Last Name' class='w-full sm:w-1/2 p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> </div> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Email Address</label> <input type='email' placeholder='Email Address' class='w-full p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Query Type</label> <div class='flex gap-4 flex-wrap'> <div class='flex items-center gap-2'> <input type='radio' id='general-enquiry' name='query-type' class='form-radio'> <label for='general-enquiry' class='text-sm text-gray-600'>General Enquiry</label> </div> <div class='flex items-center gap-2'> <input type='radio' id='support-request' name='query-type' class='form-radio'> <label for='support-request' class='text-sm text-gray-600'>Support Request</label> </div> </div> </div> <div class='flex flex-col gap-2'> <label class='text-sm font-medium text-gray-600'>Message *</label> <textarea placeholder='Message' class='w-full p-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500'></textarea> </div> <div class='flex items-center gap-2'> <input type='checkbox' id='consent' class='form-checkbox'> <label for='consent' class='text-sm text-gray-600'>I consent to being contacted by the team *</label> </div> <button type='submit' class='bg-dark-green text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-300'>Submit</button> </form> </div> </div>",
+			};
 
       const formattedHtml = await prettier.format(data.result, {
 				parser: 'html',
@@ -178,8 +178,8 @@ export default function Home() {
 			{/* --- ZONE 3 : RÉSULTAT --- generatedHtml && !isUploading  */}
 			{generatedHtml && !isUploading && (
 				<div className="space-y-6 animate-in fade-in slide-in-from-bottom-4 duration-500 ">
-					<div className="flex justify-between items-center">
-						<h3 className="text-2xl font-bold flex items-center gap-2">
+					<div className="flex justify-between items-center ">
+						<h3 className="text-2xl font-bold flex items-center gap-2 ">
 							<CheckCircle2 className="text-emerald-500" />
 							Composant généré
 						</h3>
@@ -189,26 +189,28 @@ export default function Home() {
 						</Button>
 					</div>
 
-					<div className="grid grid-cols-1 lg:grid-cols-2 gap-6 min-h-[600px]">
+					<div className="grid grid-cols-1 lg:grid-cols-2 gap-20 min-h-[600px] ">
 						{/* Colonne Gauche : Aperçu visuel (Inchangée) */}
-						<Card className="flex flex-col overflow-hidden border-slate-200 shadow-sm p-0 gap-0">
+						<Card className="flex flex-col overflow-hidden border-slate-200 shadow-sm p-0 gap-0 ">
 							<CardHeader className="bg-slate-50 border-b px-4 py-4">
 								<CardTitle className="text-sm font-medium text-slate-500 ">Aperçu visuel</CardTitle>
 							</CardHeader>
-							<div className="flex-1 min-h-[300px] lg:min-h-[500px] bg-white relative p-0 ">
+							<div className="flex-1 min-h-[300px] lg:min-h-[500px] bg-white relative p-0  ">
 								<iframe
-									srcDoc={`<!DOCTYPE html>
-                    <html>
-                      <head>
-                        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-                        <meta charset="UTF-8" />
-                      </head>
-                      <body>
-                      ${generatedHtml}
-                      </body>
-                    </html>`}
+									srcDoc={`
+										<!DOCTYPE html>
+										<html>
+										<head>
+											<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+											<meta charset="UTF-8" />
+										</head>
+										<body >
+											${generatedHtml}
+										</body>
+										</html>
+									`}
 									title="Aperçu du composant"
-									className="absolute  w-full h-full"
+									className="absolute  w-full h-full "
 									sandbox="allow-scripts"
 								/>
 							</div>
@@ -229,7 +231,7 @@ export default function Home() {
 							</CardHeader>
 
 							{/* 2. LE NOUVEAU BLOC DE CODE */}
-							<div className="flex-1 overflow-auto custom-scrollbar text-sm">
+							<div className="flex-1 overflow-auto custom-scrollbar text-sm min-h-[300px] lg:min-h-[500px]">
 								<SyntaxHighlighter language="html" style={vscDarkPlus} wrapLines={true} wrapLongLines={true} showLineNumbers={true}>
 									{generatedHtml}
 								</SyntaxHighlighter>
