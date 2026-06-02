c5bfcdc35f9f283568641d5809f7e86c07952ead
Ychnightder <pierreychnightder561@gmail.com>
2026-06-02 03:18:10 +0200

    feat: restructure backend API and update Vercel configuration for deployment

---
+++ backend/api/index.ts
@@ -0,0 +1,14 @@
+// api/index.ts
+import Fastify from 'fastify';
+import app from '../src/app.js';
+
+
+const fastify = Fastify({ logger: true });
+
+// Initialisation globale
+const start = fastify.register(app).then(() => fastify.ready());
+
+export default async (req: any, res: any) => {
+	await start;
+	fastify.server.emit('request', req, res);
+};
